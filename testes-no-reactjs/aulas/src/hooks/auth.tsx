7b487709cee9ae7a7d63c40cc9ed6310cf695b7d
Arthur Oliveira <olvarthur@gmail.com>
2020-06-30 17:07:29 -0300

    Final auth tests

--- testes-no-reactjs/aulas/src/hooks/auth.tsx
+++ testes-no-reactjs/aulas/src/hooks/auth.tsx
@@ -92,9 +92,5 @@ export const AuthProvider: React.FC = ({ children }) => {
 export function useAuth() {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-
   return context;
 }
